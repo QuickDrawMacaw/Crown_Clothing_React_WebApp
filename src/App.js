55bffe6011ef19f56220a8a0e4b6d3f2c4b325ff
Yuki Tabata <ytabata11@gmail.com>
2022-05-14 08:28:03 +1000

    Integrated Firebase package

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router";
 
 import Home from "./components/routes/home/home.component";
 import Navigation from "./components/routes/navigation/navigation.component";
+import SignIn from './components/routes/sign-in/sign-in.component';
 
 
 const Shop = () => {
@@ -14,6 +15,7 @@ const App = () => {
       <Route path='/' element={<Navigation/>}>
         <Route index element={<Home/>}/>
         <Route path='shop' element={<Shop />}/>
+        <Route path='sign-in' element={<SignIn />}/>
       </Route>  
     </Routes>
   );
