b4a0f4cd927271606da313bb51c31bcd0b933db6
Yuki Tabata <ytabata11@gmail.com>
2022-05-13 12:34:20 +1000

    commit -m "Added Categories component"

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './categories.styles.scss';
-import CategoryItem from './components/category-item/category-item.component';
+import Categories from './components/categories/categories.component';
+
 const App = () => {
 
   const categories = [
@@ -32,14 +33,8 @@ const App = () => {
   
   return (
     <div className="categories-container">
-      {categories.map((category) => (
-        <CategoryItem key={category.id}
-         category={category}
-        />
-      ))}
-    </div>
-
-    
+      <Categories categories={categories} />
+    </div> 
   );
 }
 
