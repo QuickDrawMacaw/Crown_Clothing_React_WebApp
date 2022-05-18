2532a5122c29ae930979c39609ba28704358b2a0
Yuki Tabata <ytabata11@gmail.com>
2022-05-18 20:43:35 +1000

    Implemented Redux-saga, similar to redux thunk, it relies on generator functions to execute async/await functions

--- src/store/store.js
+++ src/store/store.js
@@ -2,9 +2,11 @@ import { compose, createStore, applyMiddleware } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import { loggerMiddleWare } from "./middleware/logger";
-import logger from "redux-logger";
+import createSagaMiddleware from "redux-saga";
+
+import { rootSaga } from "./root-saga";
+
 import { rootReducer } from "./root-reducer";
-import thunk from "redux-thunk";
 
 const persistConfig = {
   key: "root",
@@ -12,11 +14,13 @@ const persistConfig = {
   blacklist: ["user"],
 };
 
+const sagaMiddleware = createSagaMiddleware();
+
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const middleWares = [
   process.env.NODE_ENV === "production" && loggerMiddleWare,
-  thunk,
+  sagaMiddleware,
 ].filter(Boolean);
 
 const composeEnhancer =
@@ -32,5 +36,7 @@ export const store = createStore(
   undefined,
   composedEnhancers
 );
+
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
